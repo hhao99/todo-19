353ebe676f01df5c5d0d4f4e764c3c1a0c3be6ec
hhao99 <hhao99@gmail.com>
2025-08-18 12:41:46 -0500

    react app with tailwindcss initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+        <h1 className='text-4xl font-bold text-red-400 mb-4'>Vite + React</h1>
+      </div>
+    </> 
+  )
+}
+
+export default App
